b6cf5085721a118395f000fb1f6d7cae9c38fc9d
Valeri <lera.hoholeva@yandex.ru>
2024-06-22 22:50:22 +0300

    update code for section france/germany/england. Add template for pictures

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-const buttons = document.querySelectorAll(".rep_section");
+const body = document.body;
+const buttons = body.querySelectorAll(".rep_section");
 buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-    buttons.forEach((l) => l.classList.remove("active"));
-    button.classList.add("active");
+    button.addEventListener("click", (e) => {
+    const targetTab = e.target;
+    const targetTabContentId = targetTab.dataset.bsTarget;
+    const targetTabContent = document.querySelector(targetTabContentId);
+
+    buttons.forEach((button) =>{
+      button.classList.remove('active');
+      const textId = button.dataset.bsTarget;
+      const targetC= document.querySelector(textId);
+      targetC.classList.remove('active__text');
+    })
+
+    button.classList.add('active');
+    targetTabContent.classList.add('active__text');
   });
 });
